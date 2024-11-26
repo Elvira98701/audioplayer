c409312115c8d059f95559c269488eaac4d3ca62
Elvira Latypova <ellalatypova666@gmail.com>
2024-11-27 01:16:56 +0500

    Some changes

--- src/scripts/helpers/htmlElements.ts
+++ src/scripts/helpers/htmlElements.ts
@@ -1,24 +1,47 @@
 import { IHtmlElements } from "./types";
 
 export const htmlElements: IHtmlElements = {
-  tracksList: document.querySelector("#tracksList"),
-  genre: document.querySelector("#genre"),
-  group: document.querySelector("#group"),
-  track: document.querySelector("#track"),
-  image: document.querySelector("#image"),
-  playButton: document.querySelector("#play"),
-  shuffleButton: document.querySelector("#shuffle"),
-  repeatButton: document.querySelector("#repeat"),
-  volume: document.querySelector("#volume"),
-  timelineEnd: document.querySelector("#timelineEnd"),
-  closeButton: document.querySelector("#closeButton"),
-  openButton: document.querySelector("#openButton"),
-  playerBlock: document.querySelector(".player__block"),
-  rewindPrev: document.querySelector("#rewindPrev"),
-  rewindNext: document.querySelector("#rewindNext"),
-  progressLine: document.querySelector("#progressLine"),
-  line: document.querySelector("#line"),
-  canvas: document.querySelector("#visualizer"),
-  preloader: document.querySelector("#preloader"),
-  startButton: document.querySelector("#startButton"),
+  tracksList: null,
+  genre: null,
+  group: null,
+  track: null,
+  image: null,
+  playButton: null,
+  shuffleButton: null,
+  repeatButton: null,
+  volume: null,
+  timelineEnd: null,
+  closeButton: null,
+  openButton: null,
+  playerBlock: null,
+  rewindPrev: null,
+  rewindNext: null,
+  progressLine: null,
+  line: null,
+  canvas: null,
+  preloader: null,
+  startButton: null,
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  htmlElements.tracksList = document.querySelector("#tracksList");
+  htmlElements.genre = document.querySelector("#genre");
+  htmlElements.group = document.querySelector("#group");
+  htmlElements.track = document.querySelector("#track");
+  htmlElements.image = document.querySelector("#image");
+  htmlElements.playButton = document.querySelector("#play");
+  htmlElements.shuffleButton = document.querySelector("#shuffle");
+  htmlElements.repeatButton = document.querySelector("#repeat");
+  htmlElements.volume = document.querySelector("#volume");
+  htmlElements.timelineEnd = document.querySelector("#timelineEnd");
+  htmlElements.closeButton = document.querySelector("#closeButton");
+  htmlElements.openButton = document.querySelector("#openButton");
+  htmlElements.playerBlock = document.querySelector(".player__block");
+  htmlElements.rewindPrev = document.querySelector("#rewindPrev");
+  htmlElements.rewindNext = document.querySelector("#rewindNext");
+  htmlElements.progressLine = document.querySelector("#progressLine");
+  htmlElements.line = document.querySelector("#line");
+  htmlElements.canvas = document.querySelector("#visualizer");
+  htmlElements.preloader = document.querySelector("#preloader");
+  htmlElements.startButton = document.querySelector("#startButton");
+});
